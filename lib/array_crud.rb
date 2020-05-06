4a7c7dd62c162c6ac03183399da1ab2375691b6b
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-06 19:51:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,27 +3,27 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  voyager = [tuvok, janeway, chakotay, seven]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  voyager << "neelix"
 end
 
 def add_element_to_start_of_array(array, element)
-
+  voyager.unshift(paris)
 end
 
 def remove_element_from_end_of_array(array)
-
+  neelix = voyager.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  tuvok = voyager.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  
 end
 
 def retrieve_first_element_from_array(array)
