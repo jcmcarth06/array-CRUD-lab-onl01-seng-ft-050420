bdd2b6b1aa965284a637ab10c042da7e8f02b986
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-06 20:03:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,13 +27,16 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.last
 end
